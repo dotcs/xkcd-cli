07278e964976e8090f0cf5ceb10a109b08a99789
dotcs <git@dotcs.me>
2022-04-13 22:49:43 +0200

    Initial commit

---
+++ xkcd.py
@@ -0,0 +1,231 @@
+from bs4 import BeautifulSoup, Tag
+from dataclasses import dataclass, asdict
+from pathlib import Path
+from subprocess import Popen, PIPE
+from textwrap import wrap
+from typing import List, Tuple
+import json
+import os
+import requests
+import shutil
+import subprocess
+import typer
+from datetime import datetime, timedelta
+
+BASE_URL = "https://xkcd.com"
+ARCHIVE_ENDPOINT = "/archive/"
+
+app = typer.Typer()
+
+CACHE_DIR = Path("~", ".cache", "xkcd-cli").expanduser()
+CACHE_PATH = Path(CACHE_DIR, "cache.json")
+
+
+@app.callback()
+def callback():
+    """
+    xkcd terminal viewer
+
+    This tool fetches xkcd comics from upstream and allows users to grep a comic
+    by selecting it in the terminal.
+
+    It relies on fzf for fuzzy searching the comic titles and kitty to render
+    the images in the terminal.
+    """
+    pass
+
+
+def setup():
+    os.makedirs(CACHE_DIR, exist_ok=True)
+
+
+@dataclass
+class XkcdComicMeta:
+    """
+    Meta information of a xkcd comic as described in the archive.
+    """
+
+    id: int
+    href: str
+    title: str
+
+
+@dataclass
+class XkcdComic(XkcdComicMeta):
+    """
+    Full information of a xkcd comic.
+    """
+
+    img_src: str
+    subtext: str
+
+
+def fetch_xkcd_archive() -> List[XkcdComicMeta]:
+    """
+    Fetches all xkcd comic meta information from the archive
+    page: https://xkcd.com/archive/
+    Since a single page contains all historic xkcds, no pagination logic is needed.
+    """
+    r = requests.get(BASE_URL + ARCHIVE_ENDPOINT)
+    r.raise_for_status()
+    text = r.text
+
+    soup = BeautifulSoup(text, "html.parser")
+    comics: List[XkcdComicMeta] = []
+
+    container = soup.find(id="middleContainer")
+    assert isinstance(container, Tag)
+
+    for el in container.find_all("a"):
+        href = el.get("href")
+        title = el.get_text()
+        cid = int(href.replace("/", ""))
+
+        meta = XkcdComicMeta(id=cid, href=href, title=title)
+        comics.append(meta)
+
+    return comics
+
+
+def fetch_xkcd_comic(comic: XkcdComicMeta) -> XkcdComic:
+    """
+    Fetch an individual xkcd comic from upstream.
+    """
+    r = requests.get(BASE_URL + comic.href)
+    r.raise_for_status()
+    html = r.text
+    soup = BeautifulSoup(html, "html.parser")
+    comic_soup = soup.find(id="comic")
+    assert isinstance(comic_soup, Tag)
+    img = comic_soup.find("img")
+    assert isinstance(img, Tag)
+    img_src = img.get("src")
+    assert isinstance(img_src, str)
+    img_src = "https:" + img_src
+    subtext = img.get("title")
+    assert isinstance(subtext, str)
+    return XkcdComic(
+        title=comic.title,
+        id=comic.id,
+        href=comic.href,
+        img_src=img_src,
+        subtext=subtext,
+    )
+
+
+def choice_fzf(fzf_cmd: Path, comics: List[XkcdComicMeta]) -> Tuple[bytes, bytes]:
+    """
+    Uses fzf to select a comic by its title.
+    """
+    titles = [str(c.id) + ": " + c.title for c in comics]
+    content = "\n".join(titles)
+
+    p = Popen([fzf_cmd], stdin=PIPE, stdout=PIPE)
+    assert p.stdin
+    p.stdin.write(content.encode())
+    p.stdin.flush()
+    stdout, stderr = p.communicate()
+    return stdout, stderr
+
+
+@app.command()
+def update_cache(
+    cache_filename: Path = typer.Option(
+        CACHE_PATH,
+        writable=True,
+        help="Path to the cache file.",
+    )
+):
+    """
+    Updates the cache file by fetching the latest comics from the xkcd archive website.
+    """
+    dt = datetime.utcnow()
+    comics = fetch_xkcd_archive()
+
+    with open(cache_filename, "w") as f:
+        comics_dicts = dict(last_updated=dt, comics=[asdict(c) for c in comics])
+        f.write(json.dumps(comics_dicts, default=str))
+
+
+@app.command()
+def show(
+    use_kitty: bool = typer.Option(
+        "kitty" in os.getenv("TERM", "").lower(),
+        help=(
+            "Defines if the output will be optimized for the kitty terminal by "
+            "rendering the image in the terminal window"
+        ),
+    ),
+    fzf_cmd: Path = typer.Option(
+        shutil.which("fzf") or os.getenv("FZF_CMD"),
+        help="Path to the fzf tool",
+    ),
+    kitty_cmd: Path = typer.Option(
+        shutil.which("kitty") or os.getenv("KITTY_CMD"),
+        help="Path to the kitty terminal",
+    ),
+    kitty_scale_up: bool = typer.Option(
+        True, help="Scales the image up to max possible width if kitty is being used."
+    ),
+    cache: bool = typer.Option(
+        True,
+        help="""\
+        Defines if a cache should be used for listing all available xkcd comics.
+        Otherwise calls the xkcd archive endpoint to gather the list of comics
+        (slower).""",
+    ),
+    cache_filename: Path = typer.Option(
+        CACHE_PATH,
+        exists=False,
+        readable=True,
+        help="Path to the cache file.",
+    ),
+):
+    """
+    Show an individual xkcd comic.
+    """
+    if cache:
+        if not cache_filename.exists():
+            # Transparently create cache for the user if cache does not yet exist.
+            update_cache(cache_filename)
+
+        content = json.load(open(cache_filename))
+        last_updated = datetime.fromisoformat(content["last_updated"])
+        if datetime.utcnow() > (last_updated + timedelta(hours=24)):
+            # Transparently create cache for the user if the cache is older than 24h.
+            update_cache(cache_filename)
+            content = json.load(open(cache_filename))
+
+        comics = [XkcdComicMeta(**c) for c in content["comics"]]
+    else:
+        # Bypass cache and read fetched results directly
+        comics = fetch_xkcd_archive()
+
+    stdout, _ = choice_fzf(fzf_cmd, comics)
+    choice = stdout.decode("UTF-8").strip()
+    choice_title = choice.split(":")[1].strip()
+
+    meta = next(c for c in comics if c.title == choice_title)
+    comic = fetch_xkcd_comic(meta)
+
+    typer.echo(typer.style(comic.title, bold=True))
+    if not use_kitty:
+        typer.echo(comic.img_src)
+    else:
+        cmd = [
+            kitty_cmd,
+            "+kitten",
+            "icat",
+            "--align=left",
+            "--scale-up" if kitty_scale_up else None,
+            comic.img_src,
+        ]
+        # Remove any None values to have a list of PathLike objects
+        cmd_filtered: List[os.PathLike] = list(filter(lambda x: x is not None, cmd))
+        subprocess.run(cmd_filtered)
+    typer.echo("\n".join(wrap(comic.subtext, width=80)))
+
+
+if __name__ == "__main__":
+    setup()
+    app()
